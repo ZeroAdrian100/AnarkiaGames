a840c7d5c02e5b96ef25b63409e71dbbe2a43f53
ZeroAdrian100 <adrian.corpe05@gmail.com>
2026-08-02 13:29:10 -0500

    (feat):Agregando dependencias al pom

---
+++ anarkiagames/src/main/java/com/utp/anarkiagames/AnarkiagamesApplication.java
@@ -0,0 +1,13 @@
+package com.utp.anarkiagames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnarkiagamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnarkiagamesApplication.class, args);
+    }
+
+}
---
+++ anarkiagames/src/test/java/com/utp/anarkiagames/AnarkiagamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.anarkiagames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnarkiagamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
