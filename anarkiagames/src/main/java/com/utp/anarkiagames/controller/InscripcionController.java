c7bad355336bdb45266bcacff64b57ab1e85e1d3
ZeroAdrian100 <adrian.corpe05@gmail.com>
2026-09-08 23:39:10 -0500

    (feat):Agregando la lógica de las inscripciones

--- anarkiagames/src/main/java/com/utp/anarkiagames/controller/InscripcionController.java
+++ anarkiagames/src/main/java/com/utp/anarkiagames/controller/InscripcionController.java
@@ -5,7 +5,6 @@ import com.utp.anarkiagames.service.InscripcionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
