756eb7ac5935cfbd7b9e1ff4a13b27d6121afe23
ZeroAdrian100 <adrian.corpe05@gmail.com>
2026-09-09 00:04:30 -0500

    (feat):Agregando las peticiones GET públicos

--- anarkiagames/src/main/java/com/utp/anarkiagames/config/SecurityConfig.java
+++ anarkiagames/src/main/java/com/utp/anarkiagames/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.utp.anarkiagames.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -27,6 +28,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/","/auth/**")
+                                .permitAll()
+                        .requestMatchers(HttpMethod.GET,"/torneos/**")
                                 .permitAll()
                         .requestMatchers("/admin/**")
                                 .hasRole("ADMIN")
