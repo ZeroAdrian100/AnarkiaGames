36cb935329bbf316b33e42f7efced2bfa54717ac
ZeroAdrian100 <adrian.corpe05@gmail.com>
2026-09-12 00:16:03 -0500

    (bugfix):Agregando el endpoint de inscripcion

--- anarkiagames/src/main/java/com/utp/anarkiagames/controller/InscripcionController.java
+++ anarkiagames/src/main/java/com/utp/anarkiagames/controller/InscripcionController.java
@@ -5,10 +5,7 @@ import com.utp.anarkiagames.service.InscripcionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -17,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class InscripcionController {
     private final InscripcionService service;
 
+    @PostMapping("/{torneoId}/inscripciones")
     public ResponseEntity<InscripcionResponse> comprar(
             @PathVariable final Long torneoId,
             @RequestBody final InscripcionRequest request,
